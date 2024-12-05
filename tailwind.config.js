4c66c13bfc6edfbc7ec5a48247cb2cbac9ec009a
yassine <yassinechmarekh88@gmail.com>
2024-12-05 12:43:04 +0100

    resolve problem require to tailwind confing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -47,5 +49,6 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  // plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
